03b43cf45348e23b2097cfb7c86658dc2e1cde75
eric <culver.eric.s@gmail.com>
2022-04-22 14:32:00 -0400

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,9 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/main.js',
   devtool: 'inline-source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Weather App',
-    }),
-  ],
   module: {
     rules: [
       {
