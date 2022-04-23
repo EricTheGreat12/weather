22bda7fcb265e621c947891ea4d86ca09d162d57
eric <culver.eric.s@gmail.com>
2022-04-23 12:14:51 -0400

    changed funciton name

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ let city = '49546';
 let key = 'b4aeac035ee123e34ece638cad464229';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
 
-async function getWeather() {
+async function getWeatherData() {
   try {
     const response = await fetch(url, { mode: 'cors' });
     const data = await response.json();
-    console.log(data);
   } catch (err) {
     console.log('err');
   }
 }
 
-getWeather();
+let currentWeather = getWeatherData();
+console.log(currentWeather);
